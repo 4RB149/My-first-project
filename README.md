815f68f7f12e0182241d41ed8caca14393c9583e
4RB149 <albaraa72yasser@gmail.com>
2022-01-13 10:40:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-k363el
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-k363el)
---
+++ index.ts
@@ -0,0 +1,20 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let num1: number;
+let num2: number;
+let num3: number;
+let num4: number;
+let num5: number;
+let num6: number;
+let num7: number;
+let num8: number;
+num1 = 1;
+num2 = 2;
+num3 = 3;
+num4 = 4;
+num5 = 5;
+num6 = 6;
+num7 = 7;
+num8 = 8;
